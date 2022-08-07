01f3ccea99f5c875972b9f99d1476c0707e98c2f
Josphat Kipyegon <josphatkips@gmail.com>
2022-08-07 12:23:52 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,146 @@
-import logo from './logo.svg';
-import './App.css';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { useEffect, useState } from 'react';
+import { useDebounce } from 'use-debounce';
+import { usePrepareSendTransaction, useSendTransaction,useWaitForTransaction, } from 'wagmi'
+import { ethers } from "ethers";
+
+import React from 'react';
 
 function App() {
+
+  useEffect(()=>{
+
+    console.log(ethers.utils.parseEther("0.5"))
+
+  },[])
+
+  const [to, setTo] = React.useState('0x143Fa46d9e6009D17120002660B21a117d006DE1')
+  const [debouncedTo] = useDebounce(to, 500)
+
+  const [amount, setAmount]=useState(0)
+  const [debouncedValue] = useDebounce(amount, 500)
+
+  const { config } = usePrepareSendTransaction({
+    request: {
+      to: debouncedTo,
+      value: debouncedValue ? ethers.utils.parseEther(debouncedValue) : undefined,
+      
+    },
+  })
+  const { data, sendTransaction } = useSendTransaction(config)
+
+  const { isLoading, isSuccess } = useWaitForTransaction({
+    hash: data?.hash,
+  })
+
+  
+  const buyToken=()=>{
+    alert("hello")
+    sendTransaction?.()
+   
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex flex-col w-full'>
+
+      <div className="justify-end flex w-full mr-3 ">
+      <ConnectButton  />
+      </div>
+     
+
+      <div className='flex flex-row w-full'>
+        <div className='mx-auto justify-center'>
+         <div> 
+         <div class="flex justify-center flex-col">
+            <div class="mb-3 xl:w-96">
+              <label for="exampleFormControlInput1" class="form-label inline-block mb-2 text-gray-700"
+                >Receiving Address </label>
+              <input
+                type="text"
+                value={to}
+                onChange={(e)=>setTo(e.target.value)}
+                class="
+                  form-control
+                  block
+                  w-full
+                  px-3
+                  py-1.5
+                  text-base
+                  font-normal
+                  text-gray-700
+                  bg-white bg-clip-padding
+                  border border-solid border-gray-300
+                  rounded
+                  transition
+                  ease-in-out
+                  m-0
+                  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+                "
+                id="amount"
+                placeholder="0xA0Cf…251e"
+              />
+            </div>
+            <div class="mb-3 xl:w-96">
+              <label for="exampleFormControlInput1" class="form-label inline-block mb-2 text-gray-700"
+                >Amount to transfer </label>
+              <input
+                type="text"
+                value={amount}
+                onChange={(e)=>setAmount(e.target.value)}
+                class="
+                  form-control
+                  block
+                  w-full
+                  px-3
+                  py-1.5
+                  text-base
+                  font-normal
+                  text-gray-700
+                  bg-white bg-clip-padding
+                  border border-solid border-gray-300
+                  rounded
+                  transition
+                  ease-in-out
+                  m-0
+                  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+                "
+                id="amount"
+                placeholder="Amount to transfer"
+              />
+            </div>
+            <div>
+            <div class="flex space-x-2 justify-center">
+
+            {isLoading ? 
+            <>
+            <button type="button" class="bg-indigo-500 ..." disabled>
+              <svg class="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24">
+              
+              </svg>
+              Sending ...
+            </button>
+            </>
+            :
+            <button onClick={(e)=>sendTransaction?.()} disabled={!sendTransaction || !to || !amount} type="button" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Buy Token</button>
+            }
+              
+            </div>
+            </div>
+            {isSuccess && (
+              <div>
+                Successfully sent {amount} ether to {to}
+                <div>
+                  <a href={`https://etherscan.io/tx/${data?.hash}`}>Etherscan</a>
+                </div>
+              </div>
+            )}
+          </div>
+         
+         </div> 
+        </div>
+
+      </div>
+     
+       
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,10 +4,47 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import {
+  chain,
+  configureChains,
+  createClient,
+  WagmiConfig,
+} from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum,chain.ropsten,chain.rinkeby,chain.goerli,chain.kovan,chain.polygonMumbai],
+  [
+    alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }),
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+})
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <WagmiConfig client={wagmiClient}>
+    <RainbowKitProvider chains={chains}>
     <App />
+    </RainbowKitProvider>
+    </WagmiConfig>
   </React.StrictMode>
 );
 
